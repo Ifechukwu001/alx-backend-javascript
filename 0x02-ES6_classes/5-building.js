8001fc6dc4405ad8902b1da2cf47b5c9f3aa04ef
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-10 11:38:22 +0100

    Implement Abstract method in class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
